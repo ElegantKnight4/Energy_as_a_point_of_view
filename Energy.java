79542d722ca859de376cab7d48fc56eaf13e8b8d
ElegantKnight4 <zourlapanos@gmail.com>
2025-01-24 10:29:11 +0200

    Create Energy.java

---
+++ Energy.java
@@ -0,0 +1,6 @@
+//Setting Managing = 1 and chanelling = 1 we will have that E = Manage*mcmod2 = chanel*mcmod2 setting dominate = 1 we have E = dominate*mcmod2
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("E = Manage*mcmod2 = chanel*mcmod2 = dominate*mcmod2");
+    }
+}
